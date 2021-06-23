6b7538580c906d2699c3eea64052eacc53e70fce
Carlos Sousa <csousa90@gmail.com>
2021-06-23 11:59:18 +0200

    Test bench uploaded

---
+++ html/index.php
@@ -0,0 +1,14 @@
+<?php
+$host = "keytechMySQL";
+$user = "root";
+$pass = "secret";
+$db = "database1";
+try {
+    $conn = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    echo "Connected successfully";
+} catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+?>
